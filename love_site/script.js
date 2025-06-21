153ea51db40022bad78b933bd1407437b3f1015a
beingwithjanathebestthingever <trytoescapefr@gmail.com>
2025-06-21 04:32:03 +0300

    Delete love_site directory

--- love_site/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("splash").addEventListener("click", function () {
-  document.getElementById("splash").classList.add("hidden");
-  document.getElementById("main").classList.remove("hidden");
-});
