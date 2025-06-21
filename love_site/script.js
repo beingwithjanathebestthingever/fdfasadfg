15392d8b0e17fc43169b0eff02f63a21d818f04d
beingwithjanathebestthingever <trytoescapefr@gmail.com>
2025-06-21 04:29:29 +0300

    Add files via upload

---
+++ love_site/script.js
@@ -0,0 +1,4 @@
+document.getElementById("splash").addEventListener("click", function () {
+  document.getElementById("splash").classList.add("hidden");
+  document.getElementById("main").classList.remove("hidden");
+});
